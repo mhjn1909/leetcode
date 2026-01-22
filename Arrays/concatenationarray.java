cb2c5b17921c95298177f80a718a394d0de43ef7
Harshit Mahajan <harshitmahajan200@gmail.com>
2026-01-22 23:31:11 +0530

    Program to concatenate the array with itself

---
+++ Arrays/concatenationarray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class concatenationarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        System.out.println("Concatenated array:");
+        System.out.println(Arrays.toString(ans));
+
+        sc.close();
+    }
+}
