2fa421f392f4d5cb0a9ee2cb975a626bc569576d
Harshit Mahajan <harshitmahajan200@gmail.com>
2026-01-23 22:21:20 +0530

    Added program for sum of 1D array

---
+++ Arrays/sumofarray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class sumofarray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        int[] result = runningSum(nums);
+        System.out.println(Arrays.toString(result));
+    }
+    static int[] runningSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            runningSum[i] = sum;
+        }
+        return runningSum;
+    }
+}
+
